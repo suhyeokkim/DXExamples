6273a7dce3ef289390c9e8cb3be3db4b3b364d66
hermauesmorarb@gmail.com <hermauesmorarb@gmail.com>
2021-02-01 16:03:01 +0900

    force x64 build

--- build_projects.bat
+++ build_projects.bat
@@ -1,14 +1,16 @@
 @echo off
 
-set BUILDER_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBUILD.EXE
+set BUILDER_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBUILD.EXE"
+set BUILDER_OPTION="/m"
 set SUBMODULE_UPDATE=git submodule update --init
 set DIRECTXTEX_SLN=./DirectXTex.sln
 set DIRECTXTEX_PATH=./DirectXTex
 set EASTL_SLN=./EASTL.sln
 set EASTL_PATH=./EASTL
 
+set OUT_VAR=' '
 echo ::%SUBMODULE_UPDATE%
-For /f "tokens=*" %%A in ('%SUBMODULE_UPDATE%') do echo %%A
+call %SUBMODULE_UPDATE%
 
 if  %ERRORLEVEL% NEQ 0 GOTO :error 
 echo. 
@@ -16,10 +18,10 @@ echo.
 if exist %DIRECTXTEX_PATH% (
 	echo ::cd %DIRECTXTEX_PATH%
 	cd %DIRECTXTEX_PATH%
-	echo ::cmake ./
-	For /f "tokens=*" %%A in ('cmake ./') do echo %%A
-	echo ::%DEVENV_PATH% %DIRECTXTEX_SLN%
-	For /f "tokens=*" %%A in ('"%BUILDER_PATH%" %DIRECTXTEX_SLN%') do echo %%A
+	echo ::cmake -A x64 ./
+	call cmake -A x64 ./
+	echo ::%BUILDER_PATH% %BUILDER_OPTION% %DIRECTXTEX_SLN%
+	call %BUILDER_PATH% %BUILDER_OPTION% %DIRECTXTEX_SLN%
 	cd ../
 ) else (
 	echo ::dxtex not exist..
@@ -29,10 +31,10 @@ echo.
 if exist %EASTL_PATH% (
 	echo ::cd %EASTL_PATH%
 	cd %EASTL_PATH%
-	echo ::cmake ./
-	For /f "tokens=*" %%A in ('cmake ./') do echo %%A
-	echo ::%DEVENV_PATH% %EASTL_SLN%
-	For /f "tokens=*" %%A in ('"%BUILDER_PATH%" %EASTL_SLN%') do echo %%A
+	echo ::cmake -A x64 ./
+	call cmake -A x64 ./
+	echo ::%BUILDER_PATH% %BUILDER_OPTION% %EASTL_SLN%
+	call %BUILDER_PATH% %BUILDER_OPTION% %EASTL_SLN%
 	cd ../
 ) else (
 	echo ::eastl not exist..
