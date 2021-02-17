071cc0151d3c89282818ee128cd9bc02e674006a
hermauesmorarb@gmail.com <hermauesmorarb@gmail.com>
2021-02-17 12:06:58 +0900

    modify library compile Debug/Release

--- build_projects.bat
+++ build_projects.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set BUILDER_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBUILD.EXE"
-set BUILDER_OPTION="/m"
+set BUILDER_OPTION=/m
 set SUBMODULE_UPDATE=git submodule update --init
 set DIRECTXTEX_SLN=./DirectXTex.sln
 set DIRECTXTEX_PATH=./DirectXTex
@@ -20,11 +20,13 @@ if exist %DIRECTXTEX_PATH% (
 	cd %DIRECTXTEX_PATH%
 	echo ::cmake -A x64 ./
 	call cmake -A x64 ./
-	echo ::%BUILDER_PATH% %BUILDER_OPTION% %DIRECTXTEX_SLN%
-	call %BUILDER_PATH% %BUILDER_OPTION% %DIRECTXTEX_SLN%
+	echo ::%BUILDER_PATH% %DIRECTXTEX_SLN% %BUILDER_OPTION% /p:configuration=Debug
+	call %BUILDER_PATH% %DIRECTXTEX_SLN% %BUILDER_OPTION% /p:configuration=Debug
+	echo ::%BUILDER_PATH% %DIRECTXTEX_SLN% %BUILDER_OPTION% /p:configuration=Release
+	call %BUILDER_PATH% %DIRECTXTEX_SLN% %BUILDER_OPTION% /p:configuration=Release
 	cd ../
 ) else (
-	echo ::dxtex not exist..
+	echo ::DirectXTex not exist..
 )
 echo. 
 
@@ -33,11 +35,13 @@ if exist %EASTL_PATH% (
 	cd %EASTL_PATH%
 	echo ::cmake -A x64 ./
 	call cmake -A x64 ./
-	echo ::%BUILDER_PATH% %BUILDER_OPTION% %EASTL_SLN%
-	call %BUILDER_PATH% %BUILDER_OPTION% %EASTL_SLN%
+	echo ::%BUILDER_PATH% %EASTL_SLN% %BUILDER_OPTION% /p:configuration=Debug
+	call %BUILDER_PATH% %EASTL_SLN% %BUILDER_OPTION% /p:configuration=Debug
+	echo ::%BUILDER_PATH% %EASTL_SLN% %BUILDER_OPTION% /p:configuration=Release
+	call %BUILDER_PATH% %EASTL_SLN% %BUILDER_OPTION% /p:configuration=Release
 	cd ../
 ) else (
-	echo ::eastl not exist..
+	echo ::EASTL not exist..
 )
 echo. 
 
