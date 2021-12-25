8bbadd94ada4c9b57e20d3e6ca026f331ac59af0
suhyeokkim <hermaeusmorarb@gmail.com>
2021-12-25 23:45:06 +0900

    #17 윈도우 관리자 스코프

--- DX12Tutorial/winmain.cpp
+++ DX12Tutorial/winmain.cpp
@@ -22,104 +22,128 @@ struct RedirectConsoleScope
         freopen_s(&stream, "CONOUT$", "w", stdout);
         freopen_s(&stream, "CONOUT$", "w", stderr);
 
-        puts("[RedirectConsoleScope] on");
+        puts("[RedirectConsoleScope] ok");
     }
 
     ~RedirectConsoleScope()
     {
-        puts("[RedirectConsoleScope] off");
-
         FreeConsole();
+
+        puts("[RedirectConsoleScope] off");
     }
 };
 
-LRESULT CALLBACK MsgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    PAINTSTRUCT ps;
-    HDC hdc;
-
-    switch (uMsg)
-    {
-    case WM_PAINT:
-        hdc = BeginPaint(hWnd, &ps);
-        EndPaint(hWnd, &ps);
-        break;
-
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-
-    default:
-        return DefWindowProc(hWnd, uMsg, wParam, lParam);
-    }
-
-    return 0;
-}
-
-inline WNDCLASSW GetWindowClass(HINSTANCE hInstance, WNDPROC msgProc, LPCWSTR className)
+struct WindowScope
 {
     WNDCLASSW wndClass;
-    memset(&wndClass, 0, sizeof(WNDCLASS));
-    wndClass.style = CS_DBLCLKS;
-    wndClass.lpfnWndProc = msgProc;
-    wndClass.hInstance = hInstance;
-    wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
-    wndClass.lpszClassName = className;
-    return wndClass;
-}
+    WindowInstance wndInst;
+    HWND hWnd;
 
-inline HWND GetCreatedWindow(HINSTANCE hInstance, LPWNDCLASSW wndClass, LPCWSTR windowTitle, UINT width, UINT height)
-{
-    RECT rc;
-    SetRect(&rc, 0, 0, width, height);
-    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, false);
-
-    return CreateWindowW(wndClass->lpszClassName, windowTitle, WS_POPUP | WS_BORDER | WS_CAPTION | WS_SYSMENU, 100, 100, width, height, 0, (HMENU)nullptr, hInstance, 0);
-}
-
-void WindowLoop(HWND hWnd, WindowInstance& instance)
-{
-    MSG msg = { 0, };
-
-    while (msg.message != WM_QUIT)
+    HRESULT Init(HINSTANCE hInstance)
     {
-        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        // Register the windows class
+        WNDCLASSW wndClass = GetWindowClass(hInstance, MsgProc, L"Direct3DWindowClass");
+        FALSE_ERROR_MESSAGE_RETURN(RegisterClassW(&wndClass), L"fail to register window class..");
+
+        WindowInstance wndInst;
+        GetDXWindowSetting(&wndInst);
+        const WindowSetting& wndSet = wndInst.settings;
+        HWND hWnd = GetCreatedWindow(hInstance, &wndClass, wndSet.windowName, wndSet.windowWidth, wndSet.windowHeight);
+
+        ShowWindow(hWnd, SW_SHOW);
+        UpdateWindow(hWnd);
+
+        FAILED_ERROR_MESSAGE_RETURN(
+            DXEntryInit(&wndInst.dx, hInstance, hWnd, wndSet.windowWidth, wndSet.windowHeight, wndSet.maxFrameRate, true),
+            L"fail to initialize."
+        );
+
+        puts("[WindowScope] ok");
+    }
+
+    void Loop()
+    {
+        MSG msg = { 0, };
+
+        while (msg.message != WM_QUIT)
         {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-        else
-        {
-            DXEntryFrameUpdate(&instance.dx);
+            if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+            else
+            {
+                DXEntryFrameUpdate(&wndInst.dx);
+            }
         }
     }
-}
+
+    ~WindowScope()
+    {
+        DXEntryClean(&wndInst.dx);
+
+        puts("[WindowScope] off");
+    }
+
+    static LRESULT CALLBACK MsgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+    {
+        PAINTSTRUCT ps;
+        HDC hdc;
+
+        switch (uMsg)
+        {
+        case WM_PAINT:
+            hdc = BeginPaint(hWnd, &ps);
+            EndPaint(hWnd, &ps);
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        default:
+            return DefWindowProc(hWnd, uMsg, wParam, lParam);
+        }
+
+        return 0;
+    }
+
+    static WNDCLASSW GetWindowClass(HINSTANCE hInstance, WNDPROC msgProc, LPCWSTR className)
+    {
+        WNDCLASSW wndClass;
+        memset(&wndClass, 0, sizeof(WNDCLASS));
+        wndClass.style = CS_DBLCLKS;
+        wndClass.lpfnWndProc = msgProc;
+        wndClass.hInstance = hInstance;
+        wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+        wndClass.lpszClassName = className;
+        return wndClass;
+    }
+
+    static HWND GetCreatedWindow(HINSTANCE hInstance, LPWNDCLASSW wndClass, LPCWSTR windowTitle, UINT width, UINT height)
+    {
+        RECT rc;
+        SetRect(&rc, 0, 0, width, height);
+        AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, false);
+
+        return CreateWindowW(wndClass->lpszClassName, windowTitle, WS_POPUP | WS_BORDER | WS_CAPTION | WS_SYSMENU, 100, 100, width, height, 0, (HMENU)nullptr, hInstance, 0);
+    }
+};
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
     RedirectConsoleScope _;
+    WindowScope wndScope;
 
-    // Register the windows class
-    WNDCLASSW wndClass = GetWindowClass(hInstance, MsgProc, L"Direct3DWindowClass");
-    FALSE_ERROR_MESSAGE_RETURN(RegisterClassW(&wndClass), L"fail to register window class..");
-
-    WindowInstance wndInst;
-    GetDXWindowSetting(&wndInst);
-    const WindowSetting& wndSet = wndInst.settings;
-    HWND hWnd = GetCreatedWindow(hInstance, &wndClass, wndSet.windowName, wndSet.windowWidth, wndSet.windowHeight);
-
-    ShowWindow(hWnd, SW_SHOW);
-    UpdateWindow(hWnd);
-    
     FAILED_ERROR_MESSAGE_RETURN(
-        DXEntryInit(&wndInst.dx, hInstance, hWnd, wndSet.windowWidth, wndSet.windowHeight, wndSet.maxFrameRate, true),
-        L"fail to initialize."
+        wndScope.Init(hInstance), 
+        L"WindowScope init fail.."
     );
-
-    WindowLoop(hWnd, wndInst);
-    DXEntryClean(&wndInst.dx);
+    wndScope.Loop();
 
 #if defined(_DEBUG)
+    puts("[DEBUG] show logs");
     auto var = getchar();
 #endif
 
