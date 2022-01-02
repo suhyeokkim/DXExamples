d509235dbcf16e14134b228496d86226995c3363
suhyeokkim <hermaeusmorarb@gmail.com>
2022-01-02 12:23:28 +0900

    #25 테스트 : 페이지 고정 메모리 할당 검사

--- Tests/allocator.test.cpp
+++ Tests/allocator.test.cpp
@@ -1,4 +1,5 @@
 #include "allocators.h"
+#include "mempage.h"
 #include "defined_type.h"
 #include "catch.hpp"
 
@@ -24,7 +25,45 @@ TEST_CASE("test memory allocate", "[Allocator]") {
     }
 
     REQUIRE(memAllocSize(addrspace0) == total);
-    REQUIRE(memPageSize(addrspace0) == memMinPageSize());
+    REQUIRE(memPageSize(addrspace0) == memPageMinSize(false));
+
+    REQUIRE(memFree(sp[1], addrspace0));
+    sp[1] = (char*)memAlloc(sa[1], 8, 0, addrspace0);
+    REQUIRE(sp[1] != nullptr);
+
+    for (auto i = 0; i < count; i++) {
+        for (auto j = 0; j < sa[i]; j++) {
+            sp[i][j] = (char)0;
+        }
+    }
+}
+
+TEST_CASE("test memory allocate (page locked)", "[Allocator]") {
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_CHECK_ALWAYS_DF | _CRTDBG_LEAK_CHECK_DF);
+
+    auto addrspace0 = L"5678";
+    auto pageSize = memPageMinSize(true);
+
+    REQUIRE(memPageAdd(addrspace0, pageSize, true));
+
+    const int count = 4;
+    uint32 sa[count] = { 12, 16, 33, 7 };
+    char* sp[count] = { 0, };
+    uint32 total = 0;
+
+    for (auto i = 0; i < count; i++) {
+        total += sa[i];
+        sp[i] = (char*)memAlloc(sa[i], 8, 0, addrspace0);
+
+        REQUIRE(sp[i] != nullptr);
+
+        for (auto j = 0; j < sa[i]; j++) {
+            sp[i][j] = (char)0;
+        }
+    }
+
+    REQUIRE(memAllocSize(addrspace0) == total);
+    REQUIRE(memPageSize(addrspace0) == memPageMinSize(true));
 
     REQUIRE(memFree(sp[1], addrspace0));
     sp[1] = (char*)memAlloc(sa[1], 8, 0, addrspace0);
