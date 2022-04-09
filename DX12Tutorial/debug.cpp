836579749ca5c29e1245ba723113962a4a9ab082
suhyeokkim <hermaeusmorarb@gmail.com>
2022-04-09 16:46:09 +0900

    #10 #19 common 에서 debug 추출
    - directx 의존성 때문에 내림

---
+++ DX12Tutorial/debug.cpp
@@ -0,0 +1,39 @@
+#include "debug.h"
+
+
+DebugPrintScope::DebugPrintScope(const wchar_t* wcs, bool reportDx12Memory) : wcs(wcs)
+{
+#if defined(_DEBUG)
+    wprintf(L"[%s] ok\n", wcs);
+#endif
+    this->reportDx12Memory = reportDx12Memory;
+}
+
+DebugPrintScope::~DebugPrintScope()
+{
+#if defined(_DEBUG)
+    wprintf(L"[%s] off\n", wcs);
+#endif
+
+    if (reportDx12Memory) {
+        CheckLeakForD3D12();
+    }
+}
+
+#ifdef _DEBUG
+void CheckLeakForD3D12()
+{
+    HMODULE dxgidebugdll = GetModuleHandleW(L"dxgidebug.dll");
+    decltype(&DXGIGetDebugInterface) GetDebugInterface = reinterpret_cast<decltype(&DXGIGetDebugInterface)>(GetProcAddress(dxgidebugdll, "DXGIGetDebugInterface"));
+
+    IDXGIDebug* debug;
+
+    GetDebugInterface(IID_PPV_ARGS(&debug));
+
+    OutputDebugStringW(L"▽▽▽▽▽▽▽▽▽▽ Direct3D Object ref count 메모리 누수 체크 ▽▽▽▽▽▽▽▽▽▽▽\r\n");
+    debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_DETAIL);
+    OutputDebugStringW(L"△△△△△△△△△△ 반환되지 않은 IUnknown 객체가 있을경우 위에 나타납니다. △△△△△△△△△△△△\r\n");
+
+    debug->Release();
+}
+#endif
---
+++ DX12Tutorial2/debug.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdio>
+
+#include <windows.h>
+#include <dxgidebug.h>
+#include <d3d12sdklayers.h>
+
+#include "symbols.h"
+
+void CheckLeakForD3D12();
+
+struct DebugPrintScope
+{
+    const wchar_t* wcs;
+    bool reportDx12Memory;
+
+    DebugPrintScope(const wchar_t* wcs, bool reportDx12Memory = false);
+    ~DebugPrintScope();
+};
